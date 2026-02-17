e6569e588fdfcb194b6d3395a215e10dc612fb1f
mahmoud-farag <mahmoud32salamn@gmail.com>
2026-02-17 13:39:18 +0200

    initialize the frontend

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import NavBar from "./components/NavBar"
+import Home from "./pages/Home.page"
+
+function App() {
+
+  return (
+  <>
+    <NavBar />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </>
+  )
+}
+
+export default App
